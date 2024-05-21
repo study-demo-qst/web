b67cb19017f35c2af0df52890ef474ae5615d5de
zuruzirou <zuruzirou@gmail.com>
2024-05-21 23:42:16 +0900

    a

--- src/main/java/com/example/web/WebApplication.java
+++ src/main/java/com/example/web/WebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TemplateLogicApplication {
+public class WebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TemplateLogicApplication.class, args);
+		SpringApplication.run(WebApplication.class, args);
 	}
 
 }
